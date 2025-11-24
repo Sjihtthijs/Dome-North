1e8b60058eecbefa298853fe2136692599b60909
TheDutchMarof <113679198+TheDutchMarof@users.noreply.github.com>
2025-11-24 13:25:30 +0100

    Made it so that all the resources and the breakable mine tiles can be destroyed after colliding with the player 4 times

--- dome-north/Scripts/resource_b_dk.gd
+++ dome-north/Scripts/resource_b_dk.gd
@@ -5,7 +5,8 @@ const MAX_HITS := 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
