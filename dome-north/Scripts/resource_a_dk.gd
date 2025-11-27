2353e79a1692405974cc989054ab6b4c2e20ea35
TheDutchMarof <113679198+TheDutchMarof@users.noreply.github.com>
2025-11-27 14:18:57 +0100

    Gold now drops when the Resource A - Block is mined

--- dome-north/Scripts/resource_a_dk.gd
+++ dome-north/Scripts/resource_a_dk.gd
@@ -5,37 +5,27 @@ const MAX_HITS := 4
 
 var Current_time = 0.0
 var time_to_mine = 4000.0
-var start_mine = 0.0
+var start_mine = -1.0
 
 @onready var GoldObject = preload("res://Scenes/DomeKeeperScene/resource_a_gold_object.tscn")
-
-func _ready():
-	pass
 
 func _process(delta):
 	Current_time = Time.get_ticks_msec()
 	if Current_time-start_mine >= time_to_mine:
+		drop_resource()
 		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body is Player_2D:
 		start_mine = Time.get_ticks_msec()
-		print("Yes!")
-		if Current_time - start_mine >= time_to_mine:
-			queue_free()
-			drop_resource()
-			print("Resource Dropped")
+		print("PLayer is mining")
 
 func _on_area_2d_body_exited(body):
 	if body is Player_2D:
-		start_mine = Current_time
-		print("No!")
+		print("Player stopped mining")
+		start_mine = -1.0
 
-func initiate_resource():
+func drop_resource():
 	var gold = GoldObject.instantiate()
 	gold.global_position = global_position
 	get_tree().get_root().add_child(gold)
-
-func drop_resource():
-	if Current_time - start_mine >= time_to_mine:
-		initiate_resource()
