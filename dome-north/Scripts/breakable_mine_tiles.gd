8ab7c55dc171b37ab49c2f8233b6307b3905b58b
Sjihtthijs <weereennaaktgeboren@gmail.com>
2025-11-28 20:26:06 +0100

    Fixed mining and lowered movement speed

--- dome-north/Scripts/breakable_mine_tiles.gd
+++ dome-north/Scripts/breakable_mine_tiles.gd
@@ -4,25 +4,28 @@ var hit_count: int = 0
 const MAX_HITS := 4
 
 var Current_time = 0.0
-var time_to_mine = 4000.0
+var time_to_mine = 3000.0
 var start_mine = 0.0
+var mining = false
 
 func _ready():
 	pass
 
 func _process(delta):
 	Current_time = Time.get_ticks_msec()
-	if Current_time-start_mine >= time_to_mine:
+	if Current_time-start_mine >= time_to_mine and mining:
 		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body is Player_2D:
 		start_mine = Time.get_ticks_msec()
 		print("Yes!")
+		mining = true
 		if Current_time - start_mine >= time_to_mine:
 			queue_free()
 
 func _on_area_2d_body_exited(body):
 	if body is Player_2D:
 		start_mine = Current_time
+		mining = false
 		print("No!")
--- dome-north/Scripts/resource_b_dk.gd
+++ dome-north/Scripts/resource_b_dk.gd
@@ -4,20 +4,22 @@ var hit_count: int = 0
 const MAX_HITS := 4
 
 var Current_time = 0.0
-var time_to_mine = 4000.0
+var time_to_mine = 3000.0
 var start_mine = 0.0
+var mining = false
 
 func _ready():
 	pass
 
 func _process(delta):
 	Current_time = Time.get_ticks_msec()
-	if Current_time-start_mine >= time_to_mine:
+	if Current_time-start_mine >= time_to_mine and mining:
 		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body is Player_2D:
 		start_mine = Time.get_ticks_msec()
+		mining = true
 		print("Yes!")
 		if Current_time - start_mine >= time_to_mine:
 			queue_free()
@@ -25,4 +27,5 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body is Player_2D:
 		start_mine = Current_time
+		mining = false
 		print("No!")
