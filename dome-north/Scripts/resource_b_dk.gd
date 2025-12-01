e85bd5c8be545d04601b309d429714c2cf5410a2
TheDutchMarof <113679198+TheDutchMarof@users.noreply.github.com>
2025-12-01 12:38:36 +0100

    Added Collectable Iron Object

--- dome-north/Scripts/resource_b_dk.gd
+++ dome-north/Scripts/resource_b_dk.gd
@@ -8,24 +8,27 @@ var time_to_mine = 3000.0
 var start_mine = 0.0
 var mining = false
 
-func _ready():
-	pass
+@onready var IronObject = preload("res://Scenes/DomeKeeperScene/resource_b_iron_object.tscn")
 
 func _process(delta):
 	Current_time = Time.get_ticks_msec()
 	if Current_time-start_mine >= time_to_mine and mining:
+		drop_resource()
 		queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body is Player_2D:
 		start_mine = Time.get_ticks_msec()
 		mining = true
-		print("Yes!")
-		if Current_time - start_mine >= time_to_mine:
-			queue_free()
+		print("Iron Ore is being mined")
 
 func _on_area_2d_body_exited(body):
 	if body is Player_2D:
+		print("Iron Ore is not being mined")
 		start_mine = Current_time
 		mining = false
-		print("No!")
+
+func drop_resource():
+	var iron = IronObject.instantiate()
+	iron.global_position = global_position
+	get_tree().get_root().add_child(iron)
