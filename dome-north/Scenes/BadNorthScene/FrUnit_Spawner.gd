5b1b8c400dea9e3faab5d9acec2da165fd3c1e88
Sjihtthijs <119939440+Sjihtthijs@users.noreply.github.com>
2025-12-22 11:46:02 +0100

    added a version of spawning

--- dome-north/Scenes/BadNorthScene/FrUnit_Spawner.gd
+++ dome-north/Scenes/BadNorthScene/FrUnit_Spawner.gd
@@ -42,7 +42,7 @@ var SpiralOfLocations = [[ 0,  0],
  [ 3,  1]]
 
 var locats
-var height = 0
+var height = 1
 # var Meshes = Grid.get_meshes
 
 # import what night it is.
@@ -50,12 +50,12 @@ var night
 
 # Ready up scene for hostile units
 var FrUnitScn = preload("res://Scenes/BadNorthScene/Components/fr_units.tscn")
-var Finstance = FrUnitScn.instantiate()
+
 
 ### Core Loop ###
 
 func _ready():
-	locats = detspawnlocs(night)
+	locats = detspawnlocs(units)
 	spawnunits(locats, units)
 	pass
 
@@ -69,15 +69,26 @@ func detspawnlocs(units):
 	'''Determine per night when and where enemies will spawn'''
 	var Places = Array([Vector3()])
 	print(units)
-	for i in range(1,3):#len(units)):
-		var Loc = Vector3(SpiralOfLocations[i][0], SpiralOfLocations[i][1], height)
+	for i in range(1,len(units)):#):
+		var Loc = Vector3(SpiralOfLocations[i][0], height, SpiralOfLocations[i][1])
 		Places.append(Loc)
 	print(Places)
 	return Places
 
 func spawnunits(locs, Units):
 	'''Do the act of spawning of enemies on the given times and locations'''
-	for i in [units, locs]:
+	var Child
+	for i in range(len(Units)):
 		#Instantiate and add units on the given locations. Then somehow combine the stats from the unit manager to the stats of the fr unit scene
+		print(Units[i].health, Units[i].max_health, Units[i].damage)
+		
+		var Finstance = FrUnitScn.instantiate()
+		add_child(Finstance)
+		Finstance.name = "FrUnit" + "%d" %[i]
+		Child = get_node(NodePath(Finstance.name))
+		Child.hp = Units[i].health
+		Child.dmg = Units[i].damage
+		Child.position = locs[i]
+		print("Position of", Finstance.name, Child.position)
 		pass
 	pass
